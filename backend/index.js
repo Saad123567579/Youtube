c1d97839b01b432b60b51c0134257fea026468ec
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2023-08-11 23:03:45 +0500

    try

--- backend/index.js
+++ backend/index.js
@@ -8,23 +8,18 @@ import authRouter from "./router/Auth.js";
 import userRouter from "./router/User.js";
 const app = express();
 
-
 let url = "mongodb+srv://saady:429331@cluster0.azmorz9.mongodb.net/Youtube";
 const connect = () => {
-    mongoose.connect(url).then(()=>console.log("Database Connected"))
-    .catch(()=>console.log("Error in connecting to the database"));
+    mongoose.connect(url).then(() => console.log("Database Connected"))
+    .catch(() => console.log("Error in connecting to the database"));
 }
 connect();
-
-
-
 
 app.use(cors({
     origin: 'https://saadtube.vercel.app',
     credentials: true,
     methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
     allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-    exposedHeaders: 'Authorization, Access-Control-Allow-Origin', // Add the headers you want to expose
 }));
 
 app.use((req, res, next) => {
@@ -36,13 +31,8 @@ app.use((req, res, next) => {
         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     );
     
-    
-    
-    
-    
     next();
 });
-
 
 app.use(express.json());
 app.use(cookieParser());
@@ -51,8 +41,6 @@ app.use("/video",videoRouter);
 app.use("/comment",commentRouter);
 app.use("/auth",authRouter);
 
-
-
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("Server Started");
-})
+});
